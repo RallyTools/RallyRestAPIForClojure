b9929affc9b0dc7835cc584c6e0ee0bf271d8034
Adam Esterline <adam@esterlines.com>
2015-04-30 14:44:35 -0600

    Preparing for release.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.rallydev/clj-rally "0.4.0"
+(defproject com.rallydev/clj-rally "0.5.0"
   :description "A clojure library for interating with Rally's webservice API."
   :url "https://github.com/RallyTools/RallyRestAPIForClojure"
   :license {:name "MIT License"
