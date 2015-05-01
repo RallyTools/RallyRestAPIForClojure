e4a406c410578e9d099c826676c043fed9c0d367
Adam Esterline and Jake Pearson <aesterline@rallydev.com>
2015-05-01 15:55:02 -0600

    API tests now use a default-data-fn.

--- test/rally/api_test.clj
+++ test/rally/api_test.clj
@@ -7,28 +7,21 @@
             [rally.api.request :as request]))
 
 (def ^:dynamic *rest-api*)
+(def generate-string (partial random/base32 15))
 
 (defn api-fixture [f]
-  (let [rest-api (api/create-rest-api)]
+  (let [default-data (fn [type data] (merge {:name (generate-string)} data))
+        rest-api     (request/set-default-data-fn (api/create-rest-api) default-data)]
     (try
       (binding [*rest-api* rest-api]
         (f))
       (finally (api/shutdown-rest-api rest-api)))))
-
-(def generate-string (partial random/base32 15))
 
 (use-fixtures :each api-fixture)
 
 (deftest ^:integration userstory-can-be-created
   (let [userstory-name (generate-string)
         userstory      (api/create! *rest-api* :userstory {:name userstory-name})]
-    (is (= (:metadata/ref-object-name userstory) userstory-name))))
-
-(deftest ^:integration userstory-can-be-created-with-default-data
-  (let [userstory-name (generate-string)
-        default-data   (fn [type values] (merge {:name userstory-name} values))
-        rest-api       (request/set-default-data-fn *rest-api* default-data)
-        userstory      (api/create! rest-api :userstory)]
     (is (= (:metadata/ref-object-name userstory) userstory-name))))
 
 (deftest ^:integration userstory-can-be-queried-by-formatted-id
@@ -38,21 +31,21 @@
     (is (= (:metadata/ref-object-name read-userstory) userstory-name))))
 
 (deftest ^:integration find-by-formatted-id-should-handle-the-oddity-in-querying-for-artifacts
-  (let [userstory (api/create! *rest-api* :userstory {:name (generate-string)})
-        defect    (api/create! *rest-api* :defect {:name (generate-string)})
+  (let [userstory (api/create! *rest-api* :userstory)
+        defect    (api/create! *rest-api* :defect)
         artifact  (api/find-by-formatted-id *rest-api* :artifact (:formatted-id defect))]
     (is (= (:metadata/ref-object-name artifact) (:metadata/ref-object-name defect)))))
 
 (deftest ^:integration userstory-can-be-updated
   (let [userstory-name (generate-string)
-        userstory      (api/create! *rest-api* :userstory {:name (generate-string)})
+        userstory      (api/create! *rest-api* :userstory)
         _              (api/update! *rest-api* userstory {:name userstory-name})
         read-userstory (api/find-by-formatted-id *rest-api* :userstory (:formatted-id userstory))]
     (is (= (:metadata/ref-object-name read-userstory) userstory-name))))
 
 (deftest ^:integration userstory-can-be-updated-without-existing-object
   (let [userstory-name (generate-string)
-        userstory      (api/create! *rest-api* :userstory {:name (generate-string)})
+        userstory      (api/create! *rest-api* :userstory)
         _              (api/update! *rest-api* (:metadata/ref userstory) {:name userstory-name})
         read-userstory (api/find-by-formatted-id *rest-api* :userstory (:formatted-id userstory))]
     (is (= (:metadata/ref-object-name read-userstory) userstory-name))))
@@ -64,7 +57,7 @@
     (is (= (:metadata/ref-object-name read-userstory) userstory-name))))
 
 (deftest ^:integration can-delete-userstory
-  (let [userstory      (api/create! *rest-api* :userstory {:name (generate-string)})
+  (let [userstory      (api/create! *rest-api* :userstory)
         _              (api/delete! *rest-api* userstory)
         read-userstory (api/find *rest-api* (:metadata/ref userstory))]
     (is (nil? read-userstory))))
@@ -90,7 +83,7 @@
     (is (set/subset? (set (vec created-refs)) (set queried-refs)))))
 
 (deftest ^:integration query-should-handle-order-by-correctly
-  (let [created-userstories (doall (repeatedly 20 #(api/create! *rest-api* :userstory {:name (generate-string)})))
+  (let [created-userstories (doall (repeatedly 20 #(api/create! *rest-api* :userstory)))
         userstory-seq       (api/query *rest-api* :userstory {:order [:name]})
 
         userstory-names     (map :metadata/ref-object-name userstory-seq)
@@ -99,8 +92,8 @@
     (is (= userstory-names sorted-names))))
 
 (deftest ^:integration relationships-can-be-queried
-  (let [created-defects [(api/create! *rest-api* :defect {:name (generate-string)}) (api/create! *rest-api* :defect {:name (generate-string)})]
-        userstory       (api/create! *rest-api* :userstory {:name (generate-string)})
+  (let [created-defects [(api/create! *rest-api* :defect) (api/create! *rest-api* :defect)]
+        userstory       (api/create! *rest-api* :userstory)
         _               (api/update-collection! *rest-api* (:defects userstory) :add created-defects)
         defects         (api/query *rest-api* (:defects userstory))]
     (is (= (sort (map :metadata/ref-object-name created-defects)) (sort (map :metadata/ref-object-name defects))))))
@@ -108,26 +101,26 @@
 (deftest ^:integration nested-relationships-can-be-queried
   (let [parent-name (generate-string)
         parent      (api/create! *rest-api* :userstory {:name parent-name})
-        child       (api/create! *rest-api* :userstory {:name (generate-string) :parent parent})
+        child       (api/create! *rest-api* :userstory {:parent parent})
         found-child (first (api/query *rest-api* :userstory [:= :parent.name parent-name]))]
     (is (= (:metadata/ref child) (:metadata/ref found-child)))))
 
 (deftest ^:integration relationships-can-be-removed
-  (let [created-defects [(api/create! *rest-api* :defect {:name (generate-string)}) (api/create! *rest-api* :defect {:name (generate-string)})]
-        userstory       (api/create! *rest-api* :userstory {:name (generate-string)})
+  (let [created-defects [(api/create! *rest-api* :defect) (api/create! *rest-api* :defect)]
+        userstory       (api/create! *rest-api* :userstory)
         _               (api/update-collection! *rest-api* (:defects userstory) :add created-defects)
         _               (api/update-collection! *rest-api* (:defects userstory) :remove [(first created-defects)])
         defects         (api/query *rest-api* (:defects userstory))]
     (is (= [(:metadata/ref-object-name (second created-defects))] (map :metadata/ref-object-name defects)))))
 
 (deftest ^:integration parent-can-be-set-on-userstory
-  (let [parent (api/create! *rest-api* :userstory {:name (generate-string)})
-        child  (api/create! *rest-api* :userstory {:name (generate-string) :parent parent})]
+  (let [parent (api/create! *rest-api* :userstory)
+        child  (api/create! *rest-api* :userstory {:parent parent})]
     (is (= (:metadata/ref parent) (:metadata/ref (:parent child))))))
 
 (deftest ^:integration parent-can-be-updated-on-userstory
-  (let [parent        (api/create! *rest-api* :userstory {:name (generate-string)})
-        child         (api/create! *rest-api* :userstory {:name (generate-string)})
+  (let [parent        (api/create! *rest-api* :userstory)
+        child         (api/create! *rest-api* :userstory)
         updated-child (api/update! *rest-api* child {:parent parent})]
     (is (= (:metadata/ref parent) (:metadata/ref (:parent updated-child))))))
 
@@ -142,11 +135,11 @@
     (is (set/subset? (set (vec created-refs)) (set queried-refs)))))
 
 (deftest ^:integration relationships-can-be-built-from-ref
-  (let [parent (api/create! *rest-api* :userstory {:name (generate-string)})
-        child  (api/create! *rest-api* :userstory {:name (generate-string) :parent parent})]
+  (let [parent (api/create! *rest-api* :userstory)
+        child  (api/create! *rest-api* :userstory {:parent parent})]
     (= [child] (api/query *rest-api* [parent :children]))))
 
 (deftest ^:integration objects-can-be-used-queries
-  (let [parent (api/create! *rest-api* :userstory {:name (generate-string)})
-        child  (api/create! *rest-api* :userstory {:name (generate-string) :parent parent})]
+  (let [parent (api/create! *rest-api* :userstory)
+        child  (api/create! *rest-api* :userstory {:parent parent})]
     (= [child] (api/query *rest-api* :userstory [:= :parent parent]))))
